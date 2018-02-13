4fc87ba117a5055256ef1c7fbbc68a05cd7d1c90
Samuel <samuel.eiben@students.fhnw.ch>
2018-02-13 10:00:06 +0100

    scale root canvas according to screen size

--- src/pages/roots/roots.ts
+++ src/pages/roots/roots.ts
@@ -12,8 +12,7 @@ export class RootsPage {
   canvasWidth: number;
 
   constructor(public navCtrl: NavController, public plt: Platform) {
-    this.canvasHeight = 50;
-    this.canvasWidth = (plt.width() - 60) / 3;
+    this.canvasWidth = (plt.width() - 60) / 4; // canvasHeight is calculated in redrawCanvas
 
     this.nthRoot = {txt: '3rd', value: 3, input: 3};
     this.result = 0;
@@ -26,43 +25,53 @@ export class RootsPage {
   redrawCanvas() {
     let canvas = document.getElementById('nthroot_canvas') as HTMLCanvasElement;
 			if(canvas.getContext){
+				if(!this.nthRoot.input) {
+					this.nthRoot.input = NaN;
+				}
+				var rootLength = this.nthRoot.input.toString().length * 10;
+        var spaceLeftAfterRoot = 30 + 5 + rootLength * 1.1;
+        var xCoordOfEquals = (spaceLeftAfterRoot + 10);
+        var x = this.canvasWidth / 4 * 1.6;
+        var scale = x / xCoordOfEquals; // scale drawing in order to fit it into the screen
+        console.log("x: " + x + " | xCoordOfEquals: " + xCoordOfEquals + " | scale: " + scale);
+        scale = Math.min(scale, 1.5);
+
+        this.canvasHeight = 45 * scale + 5; // lowest point of canvas + 5 extra pixels
+
 				var ctx = canvas.getContext("2d");
-				canvas.width = this.canvasWidth * 3;
-        canvas.height = this.canvasHeight * 3;
-				ctx.setTransform(3, 0, 0, 3, 0, 0);
+				canvas.width = this.canvasWidth * 4;
+        canvas.height = this.canvasHeight * 4;
+				ctx.setTransform(4, 0, 0, 4, 0, 0); // transform canvas for better quality
 				ctx.clearRect(0, 0, canvas.width, canvas.height);
 				ctx.lineWidth = 1;
 				ctx.strokeStyle = "#000000";
 
 				ctx.beginPath();
-				ctx.moveTo(30, 5);
-				var rootLength;
-				if(!this.nthRoot.input) {
-					this.nthRoot.input = NaN;
-				}
-				rootLength = this.nthRoot.input.toString().length * 10;
-				ctx.lineTo(30 + 5 + rootLength * 1.1, 5);
+				ctx.moveTo(30 * scale, 5 * scale);
+				ctx.lineTo((30 + 5 + rootLength * 1.1) * scale, 5 * scale);
 				ctx.stroke();
 
 				ctx.beginPath();
-				ctx.moveTo(30, 5);
-				ctx.lineTo(15, 45);
+				ctx.moveTo(30 * scale, 5 * scale);
+        ctx.lineTo(15 * scale, 45 * scale);
 				ctx.stroke();
 
 				ctx.beginPath();
-				ctx.moveTo(15, 45);
-				ctx.lineTo(10, 30);
+				ctx.moveTo(15 * scale, 45 * scale);
+				ctx.lineTo(10 * scale, 30 * scale);
 				ctx.stroke();
 
 				ctx.beginPath();
-				ctx.moveTo(10, 30);
-				ctx.lineTo(5, 30);
+				ctx.moveTo(10 * scale, 30 * scale);
+				ctx.lineTo(5 * scale, 30 * scale);
 				ctx.stroke();
 
-				ctx.font="20px Arial";
-				ctx.fillText(String(this.nthRoot.input), 30, 35);
+        var fontSize = 20 * scale;
+				ctx.font = fontSize + "px Arial";
+				ctx.fillText(String(this.nthRoot.input), 30 * scale, 35 * scale);
 
-				ctx.font="10px Arial";
+        fontSize = 10 * scale;
+				ctx.font = fontSize + "px Arial";
 				var nLength = 1;
 				if(this.nthRoot.value) {
 					nLength = this.nthRoot.value.toString().length;
@@ -71,12 +80,12 @@ export class RootsPage {
 					this.nthRoot.value = NaN;
 				}
 
-        ctx.fillText(String(this.nthRoot.value), 30 - 10 - nLength * 5, 5 + 10);
+        ctx.fillText(String(this.nthRoot.value), (30 - 10 - nLength * 5) * scale, (5 + 10) * scale);
 
-				var spaceLeftAfterRoot = 30 + 5 + rootLength * 1.1;
-				ctx.font="20px Arial";
-				ctx.fillText("=", spaceLeftAfterRoot + 10, 35);
-				ctx.fillText(String(this.result), spaceLeftAfterRoot + 30, 35)
+        fontSize = 20 * scale;
+				ctx.font = fontSize + "px Arial";
+				ctx.fillText("=", (spaceLeftAfterRoot + 10) * scale, 35 * scale);
+				ctx.fillText(String(this.result), (spaceLeftAfterRoot + 30) * scale, 35 * scale)
     }
   }
 
